c252a74fe95361dbb32b45f7b7cdce5a3f2fb0a1
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-03-18 13:52:54 +0300

    Updated dependencies

--- make/system.mk
+++ make/system.mk
@@ -54,7 +54,15 @@ endif
 
 # Set actual architecture
 # The current architecture can be obtained by: gcc -Q --help=target
-ifeq ($(patsubst armv6%,armv6,$(BUILD_ARCH)),armv6)
+ifeq ($(BUILD_ARCH),armel)
+  override ARCHITECTURE   = $(BUILD_ARCH)
+  ARCHITECTURE_FAMILY     = generic
+  ARCHITECTURE_CFLAGS    :=
+else ifeq ($(BUILD_ARCH),armhf)
+  override ARCHITECTURE   = arm32
+  ARCHITECTURE_FAMILY     = arm32
+  ARCHITECTURE_CFLAGS    := -march=armv7-a+fp -marm
+else ifeq ($(patsubst armv6%,armv6,$(BUILD_ARCH)),armv6)
   override ARCHITECTURE   = arm32
   ARCHITECTURE_FAMILY     = arm32
   ARCHITECTURE_CFLAGS    := -march=armv6 -marm
@@ -67,9 +75,9 @@ else ifeq ($(patsubst armv7%,armv7,$(BUILD_ARCH)),armv7)
   ARCHITECTURE_FAMILY     = arm32
   ARCHITECTURE_CFLAGS    := -march=armv7-a -marm
 else ifeq ($(patsubst armv8%,armv8,$(BUILD_ARCH)),armv8)
-  override ARCHITECTURE   = aarch64
-  ARCHITECTURE_FAMILY     = aarch64
-  ARCHITECTURE_CFLAGS    := -march=armv8-a
+  override ARCHITECTURE   = arm32
+  ARCHITECTURE_FAMILY     = arm32
+  ARCHITECTURE_CFLAGS    := -march=armv7-a -marm
 else ifeq ($(patsubst aarch64%,aarch64,$(BUILD_ARCH)),aarch64)
   override ARCHITECTURE   = aarch64
   ARCHITECTURE_FAMILY     = aarch64
